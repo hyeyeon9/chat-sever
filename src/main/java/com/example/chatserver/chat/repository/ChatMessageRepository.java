c68ea26ee9e8bd6db165981875c333d1e6b1af5a
구혜연 <guhaa03@daum.net>
2025-08-02 22:53:35 +0900

    feat: 채팅방 이전 메시지 조회 기능 구현

--- src/main/java/com/example/chatserver/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/chatserver/chat/repository/ChatMessageRepository.java
@@ -5,6 +5,10 @@ import com.example.chatserver.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findByChatRoomOrderByCreateTimeAsc(ChatRoom chatRoom);
 }
